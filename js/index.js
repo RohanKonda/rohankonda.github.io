71097002658d943842bb9588099030f55f49ee93
Rohan Prashanth <rohanprashanthke@gmail.com>
2021-06-23 15:53:09 +0530

    Initial version

---
+++ js/index.js
@@ -0,0 +1,39 @@
+
+console.log($("#img1").height(),  $("#img1").width());
+
+
+
+if($("#img1").css("display") != 'none'){
+
+
+$("#Header").css("height",$("#img1").height()+"px");
+
+
+
+}else{
+  $("#Header").css("height",$("#img2").height()+"px");
+
+
+}
+
+$(window). resize(function(){
+
+  if($("#img1").css("display") != 'none'){
+
+$("#Header").css("height",$("#img1").height()+"px");
+
+}else{
+$("#Header").css("height",$("#img2").height()+"px");
+
+}
+
+})
+
+
+
+
+var navimo = document.querySelector('.navimo')
+
+$("#toogle-icon").click(function(){
+  navimo.classList.toggle("unactive")
+});
